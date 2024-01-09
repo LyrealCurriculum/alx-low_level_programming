d30313320a5fa764d2816a4c870179718371a4a8
LyrealCurriculum <lyreal.real@gmail.com>
2024-01-09 02:29:30 +0100

    Function that searches strings for matching bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * *_strpbrk - searches string for any set of bytes
  * @s: string
  * @accept: other string
- * Return: pointer to bytes in s that matches bytes in accept or NULL if not found
+ * Return: pointer to matching bytes in s & accept or NULL if not found
  */
 char *_strpbrk(char *s, char *accept)
 {
