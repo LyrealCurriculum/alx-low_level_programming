97cccf2a78764786f2e0dbe938ced7e78a3f4d0e
LyrealCurriculum <lyreal.real@gmail.com>
2024-01-10 22:51:24 +0100

    Function that checks if two strings are palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - gets length of string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * compare_string - compares each character of string
+ * @s: string
+ * @left: smallest iterator
+ * @right: largest iterator
+ * Return: integer value
+ */
+int compare_string(char *s, int left, int right)
+{
+	if (*(s + left) == *(s + right))
+	{
+		if (left == right || left == right + 1)
+			return (1);
+		return (0 + compare_string(s, left + 1, right - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if two strings are palindrome
+ * @s: string
+ * Return: 1 if palindrome or 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (compare_string(s, 0, _strlen_recursion(s - 1)));
+}
