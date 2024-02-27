bf82932f7a1fc0da468257674eb75b7c794131cf
LyrealCurriculum <lyreal.real@gmail.com>
2024-02-27 17:16:23 +0100

    Finishing touches

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
+ * main - prints exactly the given string,
  * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ *
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+ * main - prints the sizes of various data types,
+ * followed by new line.
+ *
+ * Return: 0
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n"
+			"Size of an int: %ld byte(s)\n"
+			"Size of a long int: %ld byte(s)\n"
+			"Size of a long long int: %ld byte(s)\n"
+			"Size of a float: %ld byte(s)\n", sizeof(char), sizeof(int),
+			sizeof(long int), sizeof(long long int), sizeof(float));
 	return (0);
 }
