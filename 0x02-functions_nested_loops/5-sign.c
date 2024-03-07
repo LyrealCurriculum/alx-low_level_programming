5a7051a58e8a48441b3d1c9e856128af11fde432
LyrealCurriculum <lyreal.real@gmail.com>
2024-03-07 10:16:59 +0100

    Finishing touches

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,8 @@
  * print_sign - prints the sign of a number.
  * @n: the number to be checked.
  *
- * Return: 1 if n is greater than zero; 0 if n is zero;
+ * Return: 1 if n is greater than zero;
+ * 0 if n is zero;
  * -1 if n is less than zero.
  */
 int print_sign(int n)
@@ -20,6 +21,6 @@ int print_sign(int n)
 	} else if (n < 0)
 	{
 		_putchar('-');
-	}
 		return (-1);
+	}
 }
